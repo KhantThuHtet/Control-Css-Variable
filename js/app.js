d99b0e96d887858be9e69243599f3fa1ffe00b2e
KhantThuHtet <khantthu7007@gmail.com>
2021-11-19 19:46:58 +0630

    solved err

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ const inputTags = document.querySelectorAll(".controls");
 inputTags.forEach(el=>{
     el.addEventListener('input', ()=>{
         let valPx = el.dataset.sizing || '';
+
         document.documentElement.style.setProperty(
           `--${el.id}`,
-          `${el.value}px`
+          `${el.value}${valPx}`
         );
+        console.log(el.id);
     });
 });
 
